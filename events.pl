db7f3439f88d960f980434d01f95515a9430e754
Chris Henning <saturos@gmail.com>
2014-10-14 22:13:35 -0400

    went well

--- events.pl
+++ events.pl
@@ -38,91 +38,94 @@ my $usurl = "http://chat-us.riftgame.com:8080/chatservice/zoneevent/list?shardId
 my $euurl = "http://chat-eu.riftgame.com:8080/chatservice/zoneevent/list?shardId=";
 
 my %nadc = (
-        url => "http://chat-us.riftgame.com:8080/chatservice/zoneevent/list?shardId=",
-        shortname => "na",
-        names => \%usnames,
-        ids => \@usids,
-        tz => "America/Los_Angeles",
-);
+    url => "http://chat-us.riftgame.com:8080/chatservice/zoneevent/list?shardId=",
+    shortname => "na",
+    names => \%usnames,
+    ids => \@usids,
+    tz => "America/Los_Angeles",
+    );
 my %eudc = (
-        url => "http://chat-eu.riftgame.com:8080/chatservice/zoneevent/list?shardId=",
-        shortname => "eu",
-        names => \%eunames,
-        ids => \@euids,
-        tz => "GMT",
-);
+    url => "http://chat-eu.riftgame.com:8080/chatservice/zoneevent/list?shardId=",
+    shortname => "eu",
+    names => \%eunames,
+    ids => \@euids,
+    tz => "GMT",
+    );
 
 my @dcs = ();
 push(@dcs, \%nadc);
 push(@dcs, \%eudc);
 
 foreach my $dc (@dcs) {
-        foreach my $zoneid ( $dc->{"ids"}) { print $zoneid; }
+#  foreach my $zoneid ( @{ $dc->{"ids"} } ) { print $zoneid; }
 
 }
-exit;
 foreach my $dc (@dcs) {
-        my ($temp, $filename) = tempfile("retXXXXX", TMPDIR => 1);
-        my $html = new CGI;#::Compress::Gzip;
-        #print $html->header; # HTTP header
+  my ($temp, $filename) = tempfile("retXXXXX", TMPDIR => 1, UNLINK => 0);
+  my $html = new CGI;#::Compress::Gzip;
+#print $html->header; # HTTP header
 
-        print $temp $html->start_html(
-            -title => "YARET",
-            -style => { -src => 'k.css'},
-            -head => meta({-http_equiv => 'Refresh',
-                        -content => '60'}),
-            );
-        print $temp "<center><h3>Yet Another Rift Event Tracker</h3>";
-        print $temp "<div>This one with more colors</div></center>\n";
+  print $temp $html->start_html(
+      -title => "YARET",
+      -style => { -src => 'k.css'},
+      -head => meta({-http_equiv => 'Refresh',
+        -content => '60'}),
+      );
+  print $temp "<center><h3>Yet Another Rift Event Tracker</h3>";
+  print $temp "<div>This one with more colors</div></center>\n";
 
-        my @headers = ("Shard", "Zone", "Event Name", "Elapsed Time");
-        print $temp "<table>";
+  my @headers = ("Shard", "Zone", "Event Name", "Elapsed Time");
+  print $temp "<table>";
 
-        print $temp '<caption align="bottom">';
-        print $temp '<span class="relevant">Max level content</span> / <span class="oldnews">Old content</span> + <span class="new">Event just starting</span>';
-        print $temp '</caption>';
-        print $temp "<thead><tr>\n";
-        foreach my $header (@headers) {
-                print $temp "<th>$header</th>";
-        }
+  print $temp '<caption align="bottom">';
+  print $temp '<span class="relevant">Max level content</span> / <span class="oldnews">Old content</span> + <span class="new">Event just starting</span>';
+  print $temp '</caption>';
+  print $temp "<thead><tr>\n";
+  foreach my $header (@headers) {
+    print $temp "<th>$header</th>";
+  }
 
-        print $temp "</tr></thead>\n";
+  print $temp "</tr></thead>\n";
 
-        foreach my $zoneid ( $dc->{"ids"} ) { 
+  foreach my $zoneid ( @{ $dc->{"ids"} } ) { 
+    print $temp "<tbody>\n";
+    my $site = $ua->get($dc->{"url"} . $zoneid) or die $!;
+    my $result = $json->decode($site->content);
+    print $temp "<tr><td class='label'>" .  $dc->{"names"}{$zoneid} . "</td><td></td><td></td><td></td></tr>";
+    my @text = ("", "");
+    foreach my $zone (@{ $result->{"data"} }) {
+      if ($zone->{"name"}) { 
+        my $time = floor((time - $zone->{"started"})/60);
 
-  print "<tbody>\n";
-  my $site = $ua->get("$usurl$zoneid");
-  my $result = $json->decode($site->content);
-  print "<tr><td class='label'>$usnames{$zoneid}</td><td></td><td></td><td></td></tr>";
-  my @text = ("", "");
-  foreach my $zone (@{ $result->{"data"} }) {
-    if ($zone->{"name"}) { 
-      my $time = floor((time - $zone->{"started"})/60);
+        my $class = "oldnews"; my $place = 1;
+        if ($zone->{"zone"} =~ /^(The Dendrome|Steppes of Infinity|Morban|Ashora|Kingsward)$/) { $class = "relevant"; $place = 0; }
 
-      my $class = "oldnews"; my $place = 1;
-      if ($zone->{"zone"} =~ /^(The Dendrome|Steppes of Infinity|Morban|Ashora|Kingsward)$/) { $class = "relevant"; $place = 0; }
-
-      if ($zone->{"name"} eq "Hooves and Horns") { $class .= " pony"; }
+        if ($zone->{"name"} eq "Hooves and Horns") { $class .= " pony"; }
 #      if ($zone->{"name"} eq "The Awakening") { $class .= " pony"; }
 
-      if ($time < 4) { $class .= " new"; }
+        if ($time < 4) { $class .= " new"; }
 
-      $text[$place] .= "<tr class='$class'>\n";
-      $text[$place] .= "<td></td>";
-      $text[$place] .= "<td>" . $zone->{"zone"} . "</td>";
-      $text[$place] .= "<td>" . $zone->{"name"} . "</td>";
-      $text[$place] .= "<td>" . $time . "m</td>"; 
-      $text[$place] .= "</tr>\n";
+        $text[$place] .= "<tr class='$class'>\n";
+        $text[$place] .= "<td></td>";
+        $text[$place] .= "<td>" . $zone->{"zone"} . "</td>";
+        $text[$place] .= "<td>" . $zone->{"name"} . "</td>";
+        $text[$place] .= "<td>" . $time . "m</td>"; 
+        $text[$place] .= "</tr>\n";
+      }
     }
+    print $temp $text[0];
+    print $temp $text[1];
+    print $temp "</tbody>\n";
   }
-  print $text[0];
-  print $text[1];
-  print "</tbody>\n";
+
+  print $temp "</table>\n";
+
+  my $dt = DateTime->now(time_zone => $dc->{"tz"});
+  print $temp '<p align="center"><small>Generated ' . $dt->strftime("%F %T %Z") . '</small></p>';
+
+  print $temp $html->end_html;
+
+  close $temp;
+  chmod "0644", $filename;
+  rename($filename, "/var/www/rift/" . $dc->{"shortname"} . ".html");
 }
-
-print "</table>\n";
-
-my $dt =  DateTime->now(time_zone => 'America/Los_Angeles');
-print '<p align="center"><small>Generated ' . $dt->strftime("%F %T %Z") . '</small></p>';
-
-#print $html->end_html;
